2f6cccdbb9972ad5eecd82ac85ab4abfd086ae0d
Sergei Karpunin <serezha.karpunin@gmail.com>
2023-12-06 10:38:28 +0400

    Add jwt components

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation ("org.springframework.boot:spring-boot-starter-actuator")
+    implementation ("org.springframework.boot:spring-boot-starter-security")
+    implementation ("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
