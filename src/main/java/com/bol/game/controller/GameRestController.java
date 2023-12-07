9ab3f3c08e1b2337ebb9806b06767daff1e336b2
Sergei Karpunin <serezha.karpunin@gmail.com>
2023-12-07 12:48:48 +0400

    Clean up TODO

--- src/main/java/com/bol/game/controller/GameRestController.java
+++ src/main/java/com/bol/game/controller/GameRestController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
-// TODO: Swagger
 @CrossOrigin
 @RestController
 public class GameRestController implements GameRestApi {
