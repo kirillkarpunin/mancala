fecd0ca12c6aca278bf65d8e94bb230f75fe2041
Sergei Karpunin <serezha.karpunin@gmail.com>
2023-12-09 20:40:52 +0400

    Update HealthTest

--- src/test/java/com/bol/actuator/HealthTest.java
+++ src/test/java/com/bol/actuator/HealthTest.java
@@ -1,24 +1,13 @@
 package com.bol.actuator;
 
+import com.bol.AbstractRestControllerTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class HealthTest {
-
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
+public class HealthTest extends AbstractRestControllerTest {
 
     @Test
     public void shouldReturnStatusUp() {
